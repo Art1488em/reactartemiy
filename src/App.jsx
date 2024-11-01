54c979c2f64361962b8f55ada1007fa2c572825a
Art1488em <artemdavydov401@gmail.com>
2024-11-01 16:55:13 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Wrapper from './assets/components/Wrapper/Wrapper'
+
+const App = () => {
+  return (
+    <>
+      <Wrapper/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Wrapper.css'
+import Card from '../Card/Card'
+
+const Wrapper = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <Card title='DOTA 2 ' text="5x5 turbo" num="1"/>
+        <Card title='SOLO MID' text="1x1 SOLO MID" num="2"/>
+        <Card title='CS GO' text="SOON" num="3"/>
+      </div>
+    </>
+  )
+}
+
+export default Wrapper
